367facc726a09ae2255efe5730dc7dd3d40b4bd8
FreezingTexture22 <andreisesiunin@gmail.com>
2023-03-10 14:33:16 +0100

    Stage1 completed

--- Stage1.java
+++ Stage1.java
@@ -2,6 +2,12 @@ package CoffeeMachine;
 
 public class Stage1 {
 	public static void main(String[] args) {
-		
+		System.out.printf("Starting to make a coffee\r\n"
+				+ "Grinding coffee beans\r\n"
+				+ "Boiling water\r\n"
+				+ "Mixing boiled water with crushed coffee beans\r\n"
+				+ "Pouring coffee into the cup\r\n"
+				+ "Pouring some milk into the cup\r\n"
+				+ "Coffee is ready!");
 	}
 }
